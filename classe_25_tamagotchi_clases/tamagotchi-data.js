89731558350db17761c0e6a34343014ec8f9f4cd
mx-2-d <mx.dostmann@web.de>
2026-06-16 13:11:42 +0200

    Delete classe_25_tamagotchi_clases directory

--- classe_25_tamagotchi_clases/tamagotchi-data.js
+++
@@ -1,6 +0,0 @@
-import {Tamagotchi} from "./tamagotchi.js";
-
-export const arrTamagotchis = [
-    new Tamagotchi(["./img/peluche.png", "./img/peluche_bn.PNG"]),
-    new Tamagotchi(["./img/pixel.jpg", "./img/pixel_bn.JPG"])
-]
