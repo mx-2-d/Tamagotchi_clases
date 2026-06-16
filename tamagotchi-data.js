bf170e2dd190299c1aefbef6f03a3dd99a0439bd
mx-2-d <mx.dostmann@web.de>
2026-06-16 13:23:26 +0200

    Add files via upload

--- tamagotchi-data.js
+++ tamagotchi-data.js
@@ -1,6 +1,6 @@
 import {Tamagotchi} from "./tamagotchi.js";
 
 export const arrTamagotchis = [
-    new Tamagotchi(["./img/peluche.png", "./img/peluche_bn.png"]),
+    new Tamagotchi(["./img/peluche.jpg", "./img/peluche_bn.jpg"]),
     new Tamagotchi(["./img/pixel.jpg", "./img/pixel_bn.JPG"])
 ]
